d52278825811b775243fe3670bfca2c8ab8ff74b
I.M <i.m@aiMnoMacBook-Air.local>
2021-08-05 12:40:43 +0900

    Add a Static Pages controller

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module Animalcafe
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixrures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
